0a1ed464c7433d6581e78d6c180c828c3bf3500a
Rohit Singh <rohitrajput95547@gmail.com>
2026-04-05 19:48:59 +0530

    Adapter, ViewHolder, DiffUtil, Handler, RecyclerView

--- app/src/main/java/com/example/listadapteranddiffutil/MainActivity.kt
+++ app/src/main/java/com/example/listadapteranddiffutil/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.listadapteranddiffutil
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.coroutines.delay
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +21,31 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_main)
+        val adapter = ProgrammingAdapter()
+
+        val p1 = ProgrammingItem(1,"J","Java")
+        val p2 = ProgrammingItem(2,"K","Kotlin")
+        val p3 = ProgrammingItem(3,"D","Dart")
+
+        adapter.submitList(listOf<ProgrammingItem>(p1,p2,p3))
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        recyclerView.adapter = adapter
+
+        // Helps to execute code on main thread after a certain time.
+        Handler(Looper.getMainLooper()).postDelayed(Runnable{
+
+            val p4 = ProgrammingItem(4,"R","Rust")
+            val p5 = ProgrammingItem(5,"J","JavaScript")
+            val p6 = ProgrammingItem(6,"G","Go Lang")
+            val p7 = ProgrammingItem(7,"C++","C++")
+
+            adapter.submitList(listOf<ProgrammingItem>(p4,p5,p6,p7))
+
+        }, 4000)
+
     }
 }
---
+++ app/src/main/java/com/example/listadapteranddiffutil/ProgrammingItem.kt
@@ -0,0 +1,8 @@
+package com.example.listadapteranddiffutil
+
+// by default inherits equals method.
+data class ProgrammingItem(
+    val id: Int,
+    val initial: String,
+    val name: String
+)
